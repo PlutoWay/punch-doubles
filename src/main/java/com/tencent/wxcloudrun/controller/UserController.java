578b30d712234c4676838599b3cfb19bd37ee601
PlutoWay <45782910+PlutoWay@users.noreply.github.com>
2022-08-29 22:03:25 +0800

    Update UserController.java

--- src/main/java/com/tencent/wxcloudrun/controller/UserController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
      */
   @PostMapping(value = "/api/user/insert")
   ApiResponse insert(User user) {
-    logger.info("/api/user/insert get request:{}",user);
+    logger.info("/api/user/insert get request");
     if(null == user.getOpenId()){
       return  ApiResponse.error("openId为空");
     }
